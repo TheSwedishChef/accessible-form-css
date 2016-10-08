1c4538c8935b6468668563f2075b71cf06ee6fff
Chris Lorensson <chris@yourelevation.com>
2016-10-08 16:26:49 -0600

    Improvements
    • Wrote some janky JS to handle 'duplicable' field sets, such as email
    and tel.
    • got date picker working
    • minor responsive improvements
    • chose smart form style defaults to simplify LESS & reduce options
    • added JS comments
    • unique-ified JS functions

--- js/main.js
+++ js/main.js
@@ -1,96 +1,117 @@
-function formHeight() {
-  height = $("#two form").height();
-  $("#height").html(height + "px");
-}
+//////////////////////
+// FOR PREVIEW ONLY //
+//////////////////////
 
-function formWidth() {
-  width = $("#two form").width();
-  $("#width").html(width + "px");
-}
-
-function toggleStyle() {
-  style = $(this).val();
-  if($(this).is(":checked") && style == "on") {
-    console.log("style is ON");
-    $("#two form").addClass("af-styled");
-  } else if($(this).is(":checked") && style == "off") {
-    console.log("style is OFF");
-    $("#two form").removeClass("af-styled");
+  function afFormHeight() {
+    height = $("#container").height();
+    $("#height").html(height + "px");
   }
-  formHeight();
-  formWidth();
-}
 
-function toggleBox() {
-  boxed = $(this).val();
-  if($(this).is(":checked") && boxed == "on") {
-    console.log("box is ON");
-    $("#two form").addClass("af-boxed");
-  } else if($(this).is(":checked") && boxed == "off") {
-    console.log("box is OFF");
-    $("#two form").removeClass("af-boxed");
+  function afFormWidth() {
+    width = $("#container").width();
+    $("#width").html(width + "px");
   }
-  formHeight();
-  formWidth();
-}
 
-function toggleErrors() {
-  errors = $(this).val();
-  if($(this).is(":checked") && errors == "on") {
-    console.log("errors are ON");
-    $(".af-errors").show();
-    $(".af-error-candidate").addClass("af-error");
-  } else if($(this).is(":checked") && errors == "off") {
-    console.log("errors are OFF");
-    $(".af-errors").hide();
-    $(".af-error-candidate").removeClass("af-error");
+  function afBoxingAbler() {
+    box = $("input[name='boxed']:checked").val();
+    if(box == "off") {
+      $("#two form.af-form").removeClass("af-boxed");
+      console.log("boxing disabled, class removed");
+    } else if(box == "on") {
+      $("#two form.af-form").addClass("af-boxed");
+      console.log("Boxing On");
+    }
+    afFormHeight();
+    afFormWidth();
   }
-  formHeight();
-  formWidth();
-}
 
-function updateWidth() {
-  getWidth = $(this).val();
-  width = $("#two form").width();
-  if($(this).is(":checked") && getWidth == "desktop") {
-    console.log("width DESKTOP");
-    $("#container").width("100%");
-    $("#two form").removeClass("af-mobile");
-  } else if($(this).is(":checked") && getWidth == "tablet") {
-    console.log("width TABLET");
-    $("#container").width("500px");
-    $("#two form").removeClass("af-mobile");
-  } else if($(this).is(":checked") && getWidth == "mobile") {
-    console.log("width MOBILE");
-    $("#container").width("300px");
-    $("#two form").addClass("af-mobile");
+  function afToggleStyle() {
+    style = $(this).val();
+    if($(this).is(":checked") && style == "on") {
+      $("#two form.af-form").addClass("af-styled");
+      $("input[name='boxed']").removeProp("disabled");
+      console.log("style is ON, boxing enabled");
+    } else if($(this).is(":checked") && style == "off") {
+      $("#two form.af-form").removeClass("af-styled");
+      $("input[name='boxed']").prop("disabled", "disabled");
+      console.log("style is OFF, boxing disabled");
+    }
+    afBoxingAbler();
+    afFormHeight();
+    afFormWidth();
   }
-  formHeight();
-  formWidth();
-}
 
-function toggleLabels() {
-  labels = $(this).val();
-  if($(this).is(":checked") && labels == "left") {
-    console.log("labels DEFAULT");
-    $("#two form").removeClass("af-labels-top");
-  } else if($(this).is(":checked") && labels == "top") {
-    console.log("labels TOP");
-    $("#two form").addClass("af-labels-top");
+  function afToggleErrors() {
+    errors = $(this).val();
+    if($(this).is(":checked") && errors == "on") {
+      $(".af-errors").show();
+      $(".af-error-candidate").addClass("af-error");
+      console.log("errors are ON");
+    } else if($(this).is(":checked") && errors == "off") {
+      $(".af-errors").hide();
+      $(".af-error-candidate").removeClass("af-error");
+      console.log("errors are OFF");
+    }
+    afFormHeight();
+    afFormWidth();
   }
-  formHeight();
-  formWidth();
-}
 
-$("input[name='style']").change(toggleStyle);
-$("input[name='errors']").change(toggleErrors);
-$("input[name='labels']").change(toggleLabels);
-$("input[name='boxed']").change(toggleBox);
-$("input[name='boxed']").change(formHeight);
-$("input[name='style']").change(formHeight);
-$("input[name='errors']").change(formHeight);
-$("input[name='labels']").change(formHeight);
-$("input[name='widthcontrol']").change(updateWidth);
+  function afUpdateWidth() {
+    afGetWidth = $(this).val();
+    afWidth = $("#two form").width();
+    if($(this).is(":checked") && afGetWidth == "desktop") {
+      $("#container").width("100%");
+      $("#two form").removeClass("af-mobile");
+      console.log("width DESKTOP");
+    } else if($(this).is(":checked") && afGetWidth == "tablet") {
+      $("#container").width("500px");
+      $("#two form").removeClass("af-mobile");
+      console.log("width TABLET");
+    } else if($(this).is(":checked") && afGetWidth == "mobile") {
+      $("#container").width("300px");
+      $("#two form").addClass("af-mobile");
+      console.log("width MOBILE");
+    }
+    afFormHeight();
+    afFormWidth();
+  }
 
-formHeight();
-formWidth();
+  // toggle the preview form label position class: (default: none) OR add .af-labels-top
+  function afToggleLabels() {
+    afLabels = $(this).val();
+    if($(this).is(":checked") && afLabels == "left") {
+      $("#two form").removeClass("af-labels-top");
+      console.log("labels DEFAULT");
+    } else if($(this).is(":checked") && afLabels == "top") {
+      $("#two form").addClass("af-labels-top");
+      console.log("labels TOP");
+    }
+    afFormHeight();
+    afFormWidth();
+  }
+
+  $("input[name='style']").change(afToggleStyle);
+  $("input[name='errors']").change(afToggleErrors);
+  $("input[name='labels']").change(afToggleLabels);
+  $("input[name='widthcontrol']").change(afUpdateWidth);
+  $("input[name='boxed']").change(afBoxingAbler);
+
+  afFormHeight();
+  afFormWidth();
+  afBoxingAbler();
+
+//////////////////////
+//  FOR PRODUCTION  //
+//////////////////////
+
+  // for .af-duplicable items such as TEL and EMAIL, when the "add another" button is clicked, add another
+  function afAdd() {
+    afParent = $(this).closest(".af-duplicable");
+    afCopy = afParent.eq(0).clone().addClass("af-duplicate");
+    $(this).remove();
+    afParent.after(afCopy);
+    afParent.next(".af-duplicate").find("input:not([type='button'])").val("");
+    console.log("cloned item");
+  }
+
+  $("#container").on("click", ".af-add", afAdd);
